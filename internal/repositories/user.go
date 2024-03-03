096c481d3197bd8e6d88530c88c03c091ce76029
mojtaba rakhisi <mojtaba.rakhisi@gmail.com>
2024-03-03 19:43:25 +0330

    feat add user repository

---
+++ internal/repositories/user.go
@@ -0,0 +1,13 @@
+package repositories
+
+import "gorm.io/gorm"
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *userRepository {
+	return &userRepository{
+		db: db,
+	}
+}
